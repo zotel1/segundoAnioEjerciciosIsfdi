9031c9bb7fc75ca8cbe7fd9b8d56497619f52d09
zotel1 <zotelsigel@gmail.com>
2026-07-02 02:45:56 -0300

    Actividad numero tres, ordenamiento burbuja, con orden de menor a mayor

---
+++ src/algoritmo_estructura_de_datos/guia_seis/Actividad_tres.java
@@ -0,0 +1,37 @@
+package algoritmo_estructura_de_datos.guia_seis;
+
+public class Actividad_tres {
+
+    public static void main(String[] args) {
+        // Un comercio registra las cantidades disponibles de distintos productos.
+        //cantidadesProd[35, 12, 80, 5, 20, 50]
+        //El encargado quiere visualizar primero los productos con menor stock.
+
+        int[] cantidadesProd = new int[6];
+        cantidadesProd[0] = 35;
+        cantidadesProd[1] = 12;
+        cantidadesProd[2] = 80;
+        cantidadesProd[3] = 5;
+        cantidadesProd[4] = 20;
+        cantidadesProd[5] = 50;
+
+        //cantidadesProd[35, 12, 80, 5, 20, 50]
+
+        for (int i = 0; i < cantidadesProd.length; i++) {
+            for (int j = 0; j < cantidadesProd.length - 1; j++) {
+                int varAux = 0;
+                if (cantidadesProd[j] > cantidadesProd[j + 1]) {
+                    varAux = cantidadesProd[j];
+                    cantidadesProd[j] = cantidadesProd[j + 1];
+                    cantidadesProd[j + 1] = varAux;
+                }
+            }
+        }
+
+        System.out.println("Los productos con menor stock son: ");
+
+        for (int i = 0; i < cantidadesProd.length; i++) {
+            System.out.println("El " + (i + 1) + "° producto con menos stock, solo cuenta con " +cantidadesProd[i] + " productos. ");
+        }
+    }
+}
