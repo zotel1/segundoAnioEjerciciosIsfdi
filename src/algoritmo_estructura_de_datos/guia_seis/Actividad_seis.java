c4f334480322770cc591962eb0a4d42f206dc94a
zotel1 <zotelsigel@gmail.com>
2026-07-02 03:22:49 -0300

    Actividad numero seis, ordenamiento burbuja, con orden ascendiente

---
+++ src/algoritmo_estructura_de_datos/guia_seis/Actividad_seis.java
@@ -0,0 +1,39 @@
+package algoritmo_estructura_de_datos.guia_seis;
+
+public class Actividad_seis {
+
+    public static void main(String[] args) {
+        //  Los puntajes obtenidos son:
+        //1200 800 1500 950 2000
+        //Se desea mostrar el ranking de jugadores.
+
+        int[] puntajesObtenidos = new int[5];
+        puntajesObtenidos[0] = 1200;
+        puntajesObtenidos[1] = 800;
+        puntajesObtenidos[2] = 1500;
+        puntajesObtenidos[3] = 950;
+        puntajesObtenidos[4] = 2000;
+
+        System.out.println("Los puntajes obtenidos son: ");
+        for (int i = 0; i < puntajesObtenidos.length; i++) {
+            System.out.println(puntajesObtenidos[i] + " ");
+        }
+
+        for (int i = 0; i < puntajesObtenidos.length; i++) {
+            for (int j = 0; j < puntajesObtenidos.length -1; j++) {
+                int varAux = 0;
+                if (puntajesObtenidos[j] < puntajesObtenidos[j + 1]) {
+                    varAux = puntajesObtenidos[j];
+                    puntajesObtenidos[j] = puntajesObtenidos[j + 1];
+                    puntajesObtenidos[j + 1] = varAux;
+                }
+            }
+        }
+
+        System.out.println("El ranking de los jugadores es: ");
+        for (int i = 0; i < puntajesObtenidos.length; i++) {
+            System.out.println("En " + (i + 1) + "° lugar el jugador con el puntaje " + puntajesObtenidos[i] + ".");
+        }
+
+    }
+}
